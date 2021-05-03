c5ca7889b4f59fd20fa0ea71cee782d8becf7b31
PrajRana <prajwalrana07@gmail.com>
2021-05-03 14:45:41 -0500

    checked for invalid input and added cancel button

--- copy.py
+++ copy.py
@@ -1,9 +1,8 @@
 import tkinter as tk
 import mysql.connector
 
-window = tk.Tk() # Window is the main tkinter
+window = tk.Tk()  # Window is the main tkinter
 window.title("Enrol Rental System")
-
 
 def get_data():
     newwin = tk.Toplevel(window)
@@ -11,66 +10,59 @@ def get_data():
     con = mysql.connector.connect(host="localhost", user="root", password="Birgha@1", database="Part_ten")
     c = con.cursor()
 
-    # user does not provide any input
-    if len(cust_id.get())==0 and len(cust_name.get())==0:
+    if len(cust_id.get()) == 0 and len(cust_name.get()) == 0:
         c.execute('SELECT * FROM vRentalInfo ORDER BY vRentalInfo.BalanceDue')
         answer = c.fetchall()
-        print(answer)
         data = ''
         for a in answer:
-            data +="\n"+str(a[0])+"\t"+str(a[1])+"\t" +str(a[2])+"\t"+str(a[3])+"\t"+str(a[4])+\
-                    "\t"+str(a[5])+"\t"+str(a[6])+"\t"+str(a[7])+"\t"+str(a[8])+"\t"\
-                    +str(a[9])+"\t"+str(a[10])+"\t"+('$'+str("%.2f" % a[11]))+"\n"
+            data += "\n" + str(a[0]) + "\t" + str(a[1]) + "\t" + str(a[2]) + "\t" + str(a[3]) + "\t" + str(a[4]) + \
+                    "\t" + str(a[5]) + "\t" + str(a[6]) + "\t" + str(a[7]) + "\t" + str(a[8]) + "\t" \
+                    + str(a[9]) + "\t" + str(a[10]) + "\t" + ('$' + str("%.2f" % a[11])) + "\n"
 
-        query_answer = tk.Label(newwin, text=data)
-        query_answer.grid(row=0, column=0, columnspan=2)
+        # prints query output
+        result = tk.Label(newwin, text=data)
+        result.grid(row=0, column=0, columnspan=2)
+
+        # destroy tbe current window
+        cancel_button = tk.Button(newwin, text="Cancel", command=newwin.destroy)
+        cancel_button.grid(row=3, column=1, columnspan=2)
 
         con.commit()
         con.close()
 
-    # user only provides ID
-    elif len(cust_id.get())!= 0 and len(cust_name.get())== 0:
-        c.execute("SELECT DISTINCT CusomterID,CustomerName,CONCAT('$',CAST(BalanceDue AS DECIMAL(10,2))) FROM vRentalInfo WHERE CusomterID ='"+ (cust_id.get())+"';")
+
+    elif len(cust_id.get())!= 0 and len(cust_name.get()) == 0:
+        c.execute("SELECT DISTINCT CusomterID ,CustomerName,CONCAT('$',CAST(BalanceDue AS DECIMAL(10,2))) FROM "
+                  "vRentalInfo WHERE CusomterID ='" + (cust_id.get()) + "';")
         answer = c.fetchall()
-        print(answer)
         data = ''
         for a in answer:
-            data +="\n"+ str(a[0])+"\t"+str(a[1])+"\t"+str(a[2])+"\n"
+            data += "\n" + str(a[0]) + "\t" + str(a[1]) + "\t" + str(a[2]) + "\n"
 
-        query_answer = tk.Label(newwin, text=data)
-        query_answer.grid(row=2, column=0, columnspan=2)
+        # if the input is invalid
+        if len(data) == 0:
+            message = "Invalid input. Please try again "
+
+            # print the query answer
+            result = tk.Label(newwin, text=message)
+            result.grid(row=0, column=0, columnspan=2)
+
+        # if the input is valid
+        else:
+            result = tk.Label(newwin, text=data)
+            result.grid(row=0, column=0, columnspan=1)
+
+        # destroy the current window
+        cancel_button = tk.Button(newwin, text="Cancel", command=newwin.destroy)
+        cancel_button.grid(row=3, column=1, columnspan=2)
 
         con.commit()
         con.close()
 
-    # user provides full or parital name
-    elif len(cust_id.get())== 0 and len(cust_name.get())!= 0:
-        c.execute(
-            "SELECT DISTINCT CusomterID, CustomerName, CONCAT('$', CAST(BalanceDue AS DECIMAL(10,2))) FROM vRentalInfo WHERE CustomerName LIKE '" +
-            ('%' + cust_name.get() + '%') + "';")
-        answer = c.fetchall()
-        print(answer)
-        data = ''
-        for a in answer:
-            data +="\n"+ str(a[0])+"\t"+str(a[1])+"\t"+str(a[2])+"\n"
 
-        query_answer = tk.Label(newwin, text=data)
-        query_answer.grid(row=0, column=0, columnspan=1)
-
-        con.commit()
-        con.close()
-
-    # user gives both id and full name
-    elif len(cust_id.get())!= 0 and len(cust_name.get())!= 0:
-
-        c.execute(
-            "SELECT DISTINCT CusomterID, CustomerName,CONCAT('$',CAST(BalanceDue AS DECIMAL(10,2))) FROM vRentalInfo WHERE CustomerName ='" + (
-                cust_name.get()) + "'AND CusomterID ='" + (cust_id.get()) + "';")
-
-        # user give both id and partial name
-        # c.execute(
-        #     "SELECT DISTINCT CusomterID, CustomerName,CONCAT('$',CAST(BalanceDue AS DECIMAL(10,2))) FROM vRentalInfo WHERE CustomerName LIKE '" +
-        #     ('%' + cust_name.get() + '%') + "'AND CusomterID ='" + (cust_id.get()) + "';")
+    elif len(cust_id.get()) == 0 and len(cust_name.get()) != 0:
+        c.execute( "SELECT DISTINCT CusomterID, CustomerName, CONCAT('$', CAST(BalanceDue AS DECIMAL(10,2))) FROM "
+                   "vRentalInfo WHERE CustomerName LIKE '" + ('%'+cust_name.get()+'%') + "';")
 
         answer = c.fetchall()
         print(answer)
@@ -78,16 +70,61 @@ def get_data():
         for a in answer:
             data += "\n" + str(a[0]) + "\t" + str(a[1]) + "\t" + str(a[2]) + "\n"
 
-        query_answer = tk.Label(newwin, text=data)
-        query_answer.grid(row=0, column=0, columnspan=1)
+        if len(data) == 0:
+            message = "Invalid input. Please try again "
+
+            # print the query answer
+            result = tk.Label(newwin, text=message)
+            result.grid(row=0, column=0, columnspan=2)
+
+        # if the input is valid
+        else:
+            result = tk.Label(newwin, text=data)
+            result.grid(row=0, column=0, columnspan=1)
+
+        # destroy the current window
+        cancel_button = tk.Button(newwin, text="Cancel", command=newwin.destroy)
+        cancel_button.grid(row=3, column=1, columnspan=2)
 
         con.commit()
         con.close()
 
 
+    elif len(cust_id.get()) != 0 and len(cust_name.get()) != 0:
+        c.execute("SELECT DISTINCT CusomterID, CustomerName, CONCAT('$', CAST(BalanceDue AS DECIMAL(10,2))) FROM "
+                  "vRentalInfo WHERE CustomerName LIKE '" +('%'+ cust_name.get()+'%') + "'AND CusomterID ='" +
+                  (cust_id.get()) + "';")
+        answer = c.fetchall()
+        print(answer)
+        data = ''
+        for a in answer:
+            data += "\n" + str(a[0]) + "\t" + str(a[1]) + "\t" + str(a[2]) + "\n"
+
+        # if the input is invalid
+        if len(data)==0:
+            message = "Invalid input. Please try again "
+
+            # print the query answer
+            result = tk.Label(newwin, text=message)
+            result.grid(row=0, column=0, columnspan=2)
+
+        # if the input is valid
+        else:
+            result = tk.Label(newwin, text=data)
+            result.grid(row=0, column=0, columnspan=1)
+
+        # destroy the current window
+        cancel_button = tk.Button(newwin, text="Cancel", command=newwin.destroy)
+        cancel_button.grid(row=3, column=1, columnspan=2)
+
+        con.commit()
+        con.close()
+
+        # clear the input boxes NOT WORKING
+        # cust_id.delete(1.0, END)
+        # cust_name.delete(1.0, END)
 
     # display.pack()
-
 
 
 def openDisplayWindow():
@@ -109,29 +146,22 @@ def openDisplayWindow():
     cust_name_label = tk.Label(newwin, text="Name")
     cust_name_label.grid(row=1, column=0)
 
-    submit_button = tk.Button(newwin, text="Enter Data", command=get_data)
-    submit_button.grid(row=3, column=1, columnspan=2)
+    submit_button = tk.Button(newwin, text="Submit", command=get_data)
+    submit_button.grid(row=3, column=1, columnspan=1)
 
-
-    # con = mysql.connector.connect(host="localhost", user="root", password="Birgha@1", database="Part_ten")
-    # c = con.cursor()
-    # c.execute("SELECT * FROM CUSTOMER")
-    #
-    # c.execute("SELECT CustID,Name,TotalAmount FROM CUSTOMER,RENTAL WHERE RENTAL.CustID = CUSTOMER.CustID")
-    # print(c.fetchall())
-    # con.commit()
-    # con.close()
+    cancel_button = tk.Button(newwin, text="Cancel", command=newwin.destroy)
+    cancel_button.grid(row=4, column=1, columnspan=1)
 
 def openAddWindow():
     newwin = tk.Toplevel(window)
     display = tk.Label(newwin, text="A new window !")
     display.pack()
 
+
 def openPerformWindow():
     newwin = tk.Toplevel(window)
     display = tk.Label(newwin, text="A new window !")
     display.pack()
-
 
 
 add = tk.Button(
